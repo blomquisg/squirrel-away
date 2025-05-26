15395d01fe09e24296aea54261a8b7566c004418
Greg Blomquist <gblomqui@redhat.com>
2025-05-26 14:03:32 -0400

    Moved all conversion logic inline to training.

--- inferencing/src/detector/conversion/tf2tflite.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-converter = tf.lite.TFLiteConverter.from_saved_model("experiments/trained_20250302_183521/squirrel_model.tf")
-tflite_model = converter.convert()
-with open("experiments/trained_20250302_183521/squirrel_model.tflite", "wb") as f:
-    f.write(tflite_model)
-
