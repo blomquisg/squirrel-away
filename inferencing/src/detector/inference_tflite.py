6a38eb65485cdf4af7c61c31a47b11d1b3df4cf2
Greg Blomquist <gblomqui@redhat.com>
2025-05-26 14:13:46 -0400

    Updates to inferencing logic
    1. Officially removing and abandoning anything to do with pycoral, which
       is a dead project.
    2. Moved to using ai-edge-tflite directory for inferencing.
    3. Cleaned up config, effectively copying what's in training.
       Eventually this can be unified into a single reusable config for both
       subprojects.  But, for now, copy/paste reuse.

--- inferencing/src/detector/inference_tflite.py
+++ inferencing/src/detector/inference_tflite.py
@@ -2,11 +2,13 @@ import numpy as np
 import tensorflow as tf
 import ai_edge_litert.interpreter as tflite
 
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 import os
 from sys import argv
+
+from detector_config import config as cfg
+
+config = cfg.Config()
 
 DEFAULT_DATATYPE = "test"
 
@@ -49,18 +51,14 @@ def load_dataset_with_paths(data_dir, image_size=(224, 224), batch_size=16):
     
     path_ds = tf.data.Dataset.from_tensor_slices((all_file_paths, all_labels))
 
-    def process_path(file_path, label):
+    def process_dataset_path(file_path, label):
         image = tf.io.read_file(file_path)
         image = tf.image.decode_jpeg(image, channels=3)
         image = tf.image.resize(image, image_size)
-        image = tf.cast(image, tf.float32) / 255.0
-        # Normalize to match PyTorch ImageNet mean/std
-        mean = tf.constant([0.485, 0.456, 0.406])
-        std = tf.constant([0.229, 0.224, 0.225])
-        image = (image - mean) / std
+        image = tf.cast(image, tf.uint8)
         return image, label, file_path
     
-    dataset = path_ds.map(process_path, num_parallel_calls=tf.data.AUTOTUNE)
+    dataset = path_ds.map(process_dataset_path, num_parallel_calls=tf.data.AUTOTUNE)
     dataset = dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
 
     return DatasetWithPaths(dataset, class_names)
@@ -77,10 +75,13 @@ def load_dataset(data_dir, image_size=(224, 224), batch_size=16):
 
     return load_dataset_with_paths(data_dir, image_size, batch_size)
 
-
 def run_inference_tflite(interpreter, image):
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
+
+    expected_dtype = input_details[0]["dtype"]
+    assert image.dtype == expected_dtype, f"Input dtype mismatch: got {image.dtype}, expected {expected_dtype}"
+
     interpreter.set_tensor(input_details[0]['index'], image)
 
     interpreter.invoke()
@@ -88,40 +89,53 @@ def run_inference_tflite(interpreter, image):
     output = interpreter.get_tensor(output_details[0]['index'])
     return output
 
-
-def preprocess_image(image):
-    image = tf.cast(image, tf.float32) / 255.0
-    mean = tf.constant([0.485, 0.456, 0.406])
-    std = tf.constant([0.229, 0.224, 0.225])
-    return (image - mean) / std
-
-
 def get_interpreter():
-    delegate = tflite.load_delegate("../../../google-coral/libedgetpu/out/direct/k8/libedgetpu.so.1.0")
+    delegate = tflite.load_delegate(config.libedgetpu_so)
 
     interpreter = tflite.Interpreter(
-            model_path="../../experiments/trained/20250302_183521/squirrel_model.tf/squirrel_model_float32.tflite",
+            model_path=f"{config.model_dir}/model.tflite",
             experimental_delegates=[delegate])
 
     interpreter.allocate_tensors()
     return interpreter
 
+def process_image_path(image_path):
+    image_size = (224, 224)
 
+    image = tf.io.read_file(image_path)
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, image_size)
+    image = tf.cast(image, tf.uint8)
+    image = tf.expand_dims(image, 0).numpy()
+    return image
 
-def inferencing(datatype="test"):
+# Combines some of the logic from grabbing images from a 
+# directory into a dataset (namely processing a path into
+# an image object) as well as the logic of performing 
+# inferencing on a single image
+def inference_image(image_path, interpreter=None):
+    class_names = ["no-squirrel", "squirrel"]
+    interpreter = get_interpreter() if interpreter is None else interpreter
+    image = process_image_path(image_path)
+    output = run_inference_tflite(interpreter, image)
+    predicted = class_names[np.argmax(output)]
+    print(f"Inferencing result: {image_path} is a {predicted}")
+    return predicted
+
+def inference_data_dir(datatype="test"):
     all_predictions = []
     all_labels = []
     misclassified = []
 
-    dataset = load_dataset(f"../../data/{datatype}", batch_size=40)
-    class_names = dataset.class_names
-    print(f"Class names: {class_names}")
+    data_dir = f"{config.data_dir}/{datatype}"
 
+    dataset = load_dataset(data_dir, batch_size=40)
+    class_names = dataset.class_names
+
+    interpreter = get_interpreter()
     for batch in dataset:
-        images, labels, paths = batch
-        interpreter = get_interpreter()
+        images, labels, paths = batch    
         for i in range(images.shape[0]):
-            #image = preprocess_image(images[i])
             image = tf.expand_dims(images[i], 0).numpy()
 
             output = run_inference_tflite(interpreter, image)
@@ -143,10 +157,11 @@ def inferencing(datatype="test"):
     accuracy = np.mean(np.array(all_predictions) == np.array(all_labels))
     print(f"Validation accuracy: {accuracy:.4f}")
 
-
-def main(datatype="test"):
-    inferencing(datatype)
-
 if __name__ == "__main__":
-    datatype = argv[1] if len(argv) > 1 else DEFAULT_DATATYPE
-    main(datatype)
+    inference_type = argv[1]
+    inference_location = argv[2] if len(argv) > 2 else None
+    if inference_type == "directory":
+        inference_location = DEFAULT_DATATYPE if inference_location is None else inference_location
+        inference_data_dir(inference_location)
+    else:
+        inference_image(inference_location)
---
+++ inferencing/src/detector_config/config.py
@@ -0,0 +1,62 @@
+import os
+from configparser import ConfigParser, ExtendedInterpolation
+import logging
+import sys
+from pathlib import Path
+
+class Config:
+    def __init__(self, env=None):
+        if env is None:
+            env = os.getenv("ENVIRONMENT", "dev")
+
+        self.config = ConfigParser(interpolation=ExtendedInterpolation())
+
+        DEFAULT_CONFIG_PATH = "/etc/squirrel-away/inferencing/config"
+        config_path = os.getenv("SQUIRREL_AWAY_INFERENCING_CONFIG", DEFAULT_CONFIG_PATH)
+
+        self.config.read(config_path)
+
+        self.env = env
+        self._load_settings()
+        self._create_logger()
+    
+    def _load_settings(self):
+        self.model_dir = self._get_config("model_dir")
+        self.data_dir = self._get_config("data_dir")
+        self.experiments_dir = self._get_config("experiments_dir")
+        self.libedgetpu_so = self._get_config("libedgetpu_so")
+        self.log_level = self._get_config("log_level")
+        self.log_stdout = self._get_config("log_stdout")
+
+    def _get_config(self, config_name):
+        config_value = self.config.get(self.env, config_name, fallback=self.config.get("default", config_name))
+        # some configs have path names ... resolve tilde as the user's homedir for convenience
+        config_value = os.path.expanduser(config_value)
+        return config_value
+    
+    def _create_logger(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(self.log_level)
+
+        if self.log_stdout and not self.logger.hasHandlers():
+            stdout_handler = logging.StreamHandler(sys.stdout)
+            stdout_handler.setLevel(self.log_level)
+            formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+            stdout_handler.setFormatter(formatter)
+            self.logger.addHandler(stdout_handler)
+        
+    def __repr__(self):
+        _configs  = f"env={self.env}, "
+        _configs += f"data_dir={self.model_dir}, "
+        _configs += f"experiments_dir={self.experiments_dir}, "
+        _configs += f"log_level={self.log_level}, "
+
+        return f"Config({_configs})"
+    
+if __name__ == "__main__":
+    env = os.getenv("ENVIRONMENT", "dev")
+    config = Config(env=env)
+
+    config.logger.debug("Hello!")
+
+    print(config)
